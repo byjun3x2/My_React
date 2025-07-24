eec0826705231fbc1a49cdf2d95e85d4acf4072e
byjun3x2 <byjun32@gmail.com>
2025-07-24 11:02:36 +0900

    chore: Minimal React app setup with webpack and babel (without CRA)

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist')
+        },
+        port: 3000
+    }
+};
